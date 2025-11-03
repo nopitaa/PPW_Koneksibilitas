550a49ad159b713c878d4bb367fe3e2e5fa0af59
Meisarimantiantini <meysarikarangpucung@gmail.com>
2025-11-03 19:01:16 +0700

    Menambahkan halaman beranda user

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LowonganController;
 use App\Models\Lowongan;
 
+Route::get('/beranda', function () {
+    return view('beranda');
+});
 
 Route::get('/informasi-lowongan', function () {
     return view('info_lowongan');
